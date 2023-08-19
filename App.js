f64889784b95948348b0453e2813f2f68afee64b
Oleksii Bolotin <samkiral73@gmail.com>
2023-08-19 22:03:20 +0200

    fix: delete trash from App

--- App.js
+++ App.js
@@ -34,16 +34,4 @@ const styles = StyleSheet.create({
       },
     }),
   },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
 });
